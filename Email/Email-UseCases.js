3fb9956b3f82b9166f3096ebbc78d1ab68d779ef
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-03-06 13:13:23 +0530

    [Shruti] Add. Email UC5 added

--- Email/Email-UseCases.js
+++ Email/Email-UseCases.js
@@ -7,13 +7,13 @@ class EmailValidator {
     // - Second part "bridgelabz"
     // - Mandatory "." followed by "co"
     // - Optional additional parts after "co" (e.g., ".in")
-    const emailRegex = /^abc([._+-][a-zA-Z0-9]+)*@bridgelabz\.co(\.[a-zA-Z]{2,})?$/
-      // Test the email address
+    // - Optional ".<country-code>" where the country-code is exactly two characters (e.g., .in, .us)
+    const emailRegex = /^abc([._+-][a-zA-Z0-9]+)*@bridgelabz\.co(\.[a-zA-Z]{2})?$/      // Test the email address
       if (emailRegex.test(email)) {
         console.log(`The email "${email}" is valid.`);
         return true;
       } else {
-        console.log(`The email "${email}" is invalid. It must start with "abc".`);
+        console.log(`The email "${email}" is invalid. It must start have 2 characters country code.`);
         return false;
       }
     }
@@ -23,9 +23,12 @@ class EmailValidator {
   EmailValidator.validate("abc@bridgelabz.co"); 
   EmailValidator.validate("abc.xyz@bridgelabz.co"); 
   EmailValidator.validate("abc.xyz@bridgelabz.co.in")
+  EmailValidator.validate("abc.xyz@bridgelabz.co.in"); 
+  EmailValidator.validate("abc.xyz@bridgelabz.co.us");
   // Invalid email 
   EmailValidator.validate("xyz@bridgelabz.co"); 
   EmailValidator.validate("abcd@bridgelabz.co");
   EmailValidator.validate("abc@bridgelabzcom"); 
   EmailValidator.validate("abcxyz@bridgelabz"); 
-  EmailValidator.validate("abcxyz@bridgelabz.");   
+  EmailValidator.validate("abcxyz@bridgelabz."); 
+  EmailValidator.validate("abc@bridgelabz.co.uae");  
